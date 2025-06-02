24600ff33bf032e30c47e660cac1bbf6269cc355
deandra-smith <93276718+deandra-smith@users.noreply.github.com>
2025-06-02 17:52:07 -0400

    added our sign in component and firebase

--- src/App.js
+++ src/App.js
@@ -1,38 +1,28 @@
-import CategoriesDirectory from './components/categories-directory/categories-directory';
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/home/home';
+import Navigation from './routes/navigation/navigation';
+import SignIn from './routes/sign-in/sign-in';
+
+
+
+  const Shop = () => {
+    return (
+      <div>
+        <h1>I am the shop page</h1>
+      </div>
+    );
+  }
 
 const App = () => {
-  const categories = [
-  {
-    "id": 1,
-    "title": "hats",
-    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-  },
-  {
-    "id": 2,
-    "title": "jackets",
-    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-  },
-  {
-    "id": 3,
-    "title": "sneakers",
-    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-  },
-  {
-    "id": 4,
-    "title": "womens",
-    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-  },
-  {
-    "id": 5,
-    "title": "mens",
-    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-  }
-]
-
   return (
-    <>
-      <CategoriesDirectory categories={categories} />;
-    </>
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
+      </Route>
+    </Routes>
+
   );
 }
 
